287d12358fa3f339a1067a842c55532153477219
Alonzo Vargas <alonzo@oncodesolutions.com>
2021-08-05 18:18:28 -0600

    Reestructuring files app

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,21 @@
+import "./Home.css"
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>HackFS - 2021</h1>
+        <h2>Publisher Project</h2>
+        <p>
+          Made with love for{" "}
+          <a className="App-link" href="twoscomplement.io">
+            2C
+          </a>{" "}
+          indies.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default Home
